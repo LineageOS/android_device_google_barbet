f2a988faa9e2a03d619fa92189f1f73ab3fe0d93
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-14 00:36:02 -0700

    barbet: Set up proprietary files extraction
    * Blobs that can be built from source code are built from source.
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Alessandro Astone <ales.astone@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Christopher N. Hesse <raymanfx@gmail.com>
    Co-authored-by: Dan Pasanen <dan.pasanen@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Co-authored-by: Josh Chasky <jchasky@gmail.com>
    Co-authored-by: "Josh Fox (XlxFoXxlX)" <joshfox87@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I17f79b417a8f0d022bec9c6b5f37d03b842d43e1

--- lineage_barbet.mk
+++ lineage_barbet.mk
@@ -26,3 +26,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="barbet-user 12 SQ1A.211205.008 7888514 release-keys"
 
 BUILD_FINGERPRINT := google/barbet/barbet:12/SQ1A.211205.008/7888514:user/release-keys
+
+$(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
