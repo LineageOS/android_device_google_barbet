50dbd4907c8c1b8f0bf19d97bd3df72ac2d5b10f
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    barbet: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ lineage_barbet.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/barbet/aosp_barbet.mk)
+
+include device/google/barbet/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 5a
+PRODUCT_NAME := lineage_barbet
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=barbet \
+    PRIVATE_BUILD_DESC="barbet-user 12 SQ1A.211205.008 7888514 release-keys"
+
+BUILD_FINGERPRINT := google/barbet/barbet:12/SQ1A.211205.008/7888514:user/release-keys
