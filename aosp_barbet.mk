eba1f9f97b3c26de435b2f736098ea5dca09754e
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 16:25:29 -0700

    barbet: Drop PRODUCT_BUILD_SUPER_PARTITION
     * Super image is not generated by a default 'm' build,
       but we at least want it for dist builds.
    
    Change-Id: Ia12a5314632509fcd309d51ddf757efdc37499c3

--- aosp_barbet.mk
+++ aosp_barbet.mk
@@ -46,9 +46,6 @@ $(call inherit-product, device/google/barbet/device-barbet.mk)
 $(call inherit-product-if-exists, vendor/google_devices/barbet/proprietary/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/barbet/prebuilts/device-vendor-barbet.mk)
 
-# Don't build super.img.
-PRODUCT_BUILD_SUPER_PARTITION := false
-
 # b/113232673 STOPSHIP deal with Qualcomm stuff later
 # PRODUCT_RESTRICT_VENDOR_FILES := all
 
