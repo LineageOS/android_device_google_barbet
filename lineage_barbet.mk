af69d67236290e28a4b48749e9206c8f3ad87d9b
Michael Bestas <mkbestas@lineageos.org>
2023-07-08 13:17:34 +0300

    Update fingerprint/build description from TQ3A.230705.001
    Change-Id: I5ff701c04b218796ec3671d05f625109a2ade531

--- lineage_barbet.mk
+++ lineage_barbet.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 13 TQ3A.230605.011 10161073 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 13 TQ3A.230705.001 10216780 release-keys"
 
-BUILD_FINGERPRINT := google/barbet/barbet:13/TQ3A.230605.011/10161073:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:13/TQ3A.230705.001/10216780:user/release-keys
 
 $(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
