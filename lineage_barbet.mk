df69251e02178f1efdf8eeffeb11581f0eb543c3
Michael Bestas <mkbestas@lineageos.org>
2024-05-08 02:27:07 +0300

    Update fingerprint/build description from AP1A.240505.004
    Change-Id: I1cb20e979784cfcd24aef4881ab9e3142d57e3ab

--- lineage_barbet.mk
+++ lineage_barbet.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 14 AP1A.240405.002 11480754 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 14 AP1A.240505.004 11583682 release-keys"
 
-BUILD_FINGERPRINT := google/barbet/barbet:14/AP1A.240405.002/11480754:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:14/AP1A.240505.004/11583682:user/release-keys
 
 $(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
