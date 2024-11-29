dafe697ddb53198453dda67ea188d6caa077a350
Michael Bestas <mkbestas@lineageos.org>
2024-11-30 00:39:08 +0200

    barbet: Remove proprietary files regen support
    Change-Id: I1fbc6891dcd09652126b8c5ed28877c4ebd58165

--- barbet/extract-files.py
+++ barbet/extract-files.py
@@ -11,7 +11,6 @@ from extract_utils.extract_pixel import (
     pixel_factory_image_regex,
     pixel_firmware_regex,
 )
-from extract_utils.file import FileArgs, FileList
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -77,72 +76,12 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    add_generated_carriersettings_file=True,
     add_firmware_proprietary_file=True,
     extract_fns=extract_fns,
 )
 
-
-def fix_vendor_file_list(file_list: FileList):
-    file_list.get_file(
-        'vendor/app/adreno_graphics_driver/adreno_graphics_driver.apk'
-    ).set_arg(FileArgs.PRESIGNED, True)
-
-    file_list.get_file('vendor/app/CneApp/CneApp.apk').set_arg(
-        FileArgs.REQUIRED, 'CneApp.libvndfwk_detect_jni.qti_symlink'
-    )
-
-    file_list.get_file('vendor/lib/egl/libEGL_adreno.so').set_arg(
-        FileArgs.SYMLINK, 'vendor/lib/libEGL_adreno.so'
-    )
-    file_list.get_file('vendor/lib/egl/libGLESv2_adreno.so').set_arg(
-        FileArgs.SYMLINK, 'vendor/lib/libGLESv2_adreno.so'
-    )
-    file_list.get_file('vendor/lib/egl/libq3dtools_adreno.so').set_arg(
-        FileArgs.SYMLINK, 'vendor/lib/libq3dtools_adreno.so'
-    )
-    file_list.get_file('vendor/lib64/egl/libEGL_adreno.so').set_arg(
-        FileArgs.SYMLINK, 'vendor/lib64/libEGL_adreno.so'
-    )
-    file_list.get_file('vendor/lib64/egl/libGLESv2_adreno.so').set_arg(
-        FileArgs.SYMLINK, 'vendor/lib64/libGLESv2_adreno.so'
-    )
-    file_list.get_file('vendor/lib64/egl/libq3dtools_adreno.so').set_arg(
-        FileArgs.SYMLINK, 'vendor/lib64/libq3dtools_adreno.so'
-    )
-
-    # 32bit libmmcamera_faceproc is unable to resolved the following symbols:
-    # __aeabi_memcpy@LIBC_PRIVATE, __aeabi_memset@LIBC_PRIVATE, __gnu_Unwind_Find_exidx@LIBC_PRIVATE
-    # lowi-server, libcne, libwqe depend on libwpa_client, which is a gnu makefile target
-    disable_checkelf_file_paths = [
-        'vendor/bin/lowi-server',
-        'vendor/lib/libcne.so',
-        'vendor/lib/libmmcamera_faceproc.so',
-        'vendor/lib/libwqe.so',
-        'vendor/lib64/libcne.so',
-        'vendor/lib64/libmmcamera_faceproc.so',
-        'vendor/lib64/libwqe.so',
-    ]
-    for file_path in disable_checkelf_file_paths:
-        file_list.get_file(file_path).set_arg(FileArgs.DISABLE_CHECKELF, True)
-
-    module_suffix_file_paths = [
-        'vendor/lib/vendor.qti.hardware.tui_comm@1.0.so',
-        'vendor/lib/vendor.qti.imsrtpservice@3.0.so',
-        'vendor/lib64/vendor.qti.hardware.tui_comm@1.0.so',
-        'vendor/lib64/vendor.qti.imsrtpservice@3.0.so',
-    ]
-
-    for file_path in module_suffix_file_paths:
-        file_list.get_file(file_path).set_arg(FileArgs.MODULE_SUFFIX, '_vendor')
-
-
-module.add_generated_proprietary_file(
-    'proprietary-files-vendor.txt',
-    partition='vendor',
-    skip_file_list_name='skip-files-vendor.txt',
-    fix_file_list=fix_vendor_file_list,
-)
+module.add_proprietary_file('proprietary-files-carriersettings.txt')
+module.add_proprietary_file('proprietary-files-vendor.txt')
 
 if __name__ == '__main__':
     utils = ExtractUtils.device(module)
